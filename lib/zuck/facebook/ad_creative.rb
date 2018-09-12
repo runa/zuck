0a1de130f55733711a0a4b6d329608277b1e265a
martin sarsale <martin@properati.com>
2018-09-12 18:33:44 -0300

    removed obsoleted fields from AdCreative

--- lib/zuck/facebook/ad_creative.rb
+++ lib/zuck/facebook/ad_creative.rb
@@ -6,7 +6,6 @@ module Zuck
     known_keys :body,
                :call_to_action_type,
                :image_crops,
-               :image_file,
                :image_hash,
                :image_url,
                :link_url,
@@ -17,7 +16,6 @@ module Zuck
                :object_url,
                :title,
                :url_tags,
-               :multi_share_optimized,
                :id
 
     parent_object :ad_group
